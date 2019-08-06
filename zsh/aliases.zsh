6efaf9e3c562fc631226da3d406497b9263233c9
Isac Cajá <isac.caja@gmail.com>
2019-08-05 21:00:41 -0300

    Create save-secrets alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,5 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias save-secrets='cd ${HOME}/.dotfiles && git add ./pass && git commit -m "Update/Add secrets" && git push && cd -'
